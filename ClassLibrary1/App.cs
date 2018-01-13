2644213c24d478c03e0516ebf45ae47d44ef90a9
Edward Brey <edward.brey@toolsay.com>
2018-01-12 23:25:52 -0600

    Add project files.

---
+++ ClassLibrary1/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ClassLibrary1
+{
+    public class App : Application
+	{
+		public App()
+		{
+			MainPage = new Page1();
+		}
+    }
+}
---
+++ ClassLibrary1/Page1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClassLibrary1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1()
+		{
+			InitializeComponent();
+			listView.ItemsSource = new[] { new ListItem { Hello = "Hello world 3" } };
+		}
+	}
+}
